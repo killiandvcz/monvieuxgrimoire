e0049cc9eb4a4d0b6c7a820fecfb356e462b2d7d
Killian Di Vincenzo <hello@killiandvcz.fr>
2025-03-14 14:38:34 +0100

    .env

--- back/src/config/env.config.js
+++ back/src/config/env.config.js
@@ -1,5 +1,5 @@
 export const env = {
-    MONGO_URL: process.env.MONGO_URL,
+    MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/monvieuxgrimoire',
     PORT: process.env.PORT || 8080,
     JWT_SECRET: process.env.JWT_SECRET || 'secret',
 }
